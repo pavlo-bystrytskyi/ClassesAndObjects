5a59cc5b78865921bcee6deed93670698055188c
Pavlo Bystrytskyi <pavlo.bystrytskyi@gmail.com>
2024-09-05 12:03:08 +0200

    Book::toString and Book::hashCode added.

--- src/main/java/org/example/Book.java
+++ src/main/java/org/example/Book.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Objects;
+
 public class Book {
     private String title;
     private String author;
@@ -33,6 +35,19 @@ public class Book {
 
     public void setIsbn(String isbn) {
         this.isbn = isbn;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(title, book.title) && Objects.equals(author, book.author) && Objects.equals(isbn, book.isbn);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, isbn);
     }
 
     public String toString() {
