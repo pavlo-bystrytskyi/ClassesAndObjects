9234ab658390babf49b1010eb0c4c17436c04ef8
Pavlo Bystrytskyi <pavlo.bystrytskyi@gmail.com>
2024-09-05 12:03:53 +0200

    Library books field changed to private.

--- src/main/java/org/example/Library.java
+++ src/main/java/org/example/Library.java
@@ -1,25 +1,21 @@
 package org.example;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class Library {
-     ArrayList<Book> books;
+    private final ArrayList<Book> books;
 
     Library(ArrayList<Book> books) {
         this.books = books;
     }
 
-    public ArrayList<Book>  getBooks() {
+    public ArrayList<Book> getBooks() {
         return books;
     }
 
     public void addBook(Book book) {
         books.add(book);
     }
-
-
 
     public void removeBook(Book book) {
         books.remove(book);
